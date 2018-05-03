8bc515f10d78727af2f94be47f20b61a5733d48f
michele <rmichele89@gmail.com>
2018-05-03 12:24:50 +0200

    yfsfsdg

--- EasyCourse/src/easycourse/EasyCourse.java
+++ EasyCourse/src/easycourse/EasyCourse.java
@@ -20,31 +20,41 @@ import javax.ws.rs.core.Response;
 
 
 
-@Path("/corsi")
+@Path("/{id_uni}")
 public class EasyCourse {
 
-	private static  ArrayList<Corso> listCorsi;
+	
+	private static HashMap<String,Corso> listCorsi;
+	private static HashMap<String,Docente> mapDocenti;
+	private static HashMap<Integer,Aula> mapAule;
+	
+	
 	
  	
 	public EasyCourse() {
 		if (listCorsi == null) {
 			
-			listCorsi = new ArrayList<Corso>();
+			listCorsi = new HashMap<String,Corso>();
+			mapDocenti = new HashMap<String,Docente>();
+			mapAule = new HashMap<Integer,Aula>();
+			
 			Corso c = new Corso("1", "Ingegneria del software", new Docente("Pasquale", "Foggia", "1"), 1, 1);
 			HashMap<Slot, Aula> h = new HashMap<Slot, Aula>();
 			Aula a = new Aula(1, "M");
 			h.put(new Slot("Giovedì", 9, 13), a);
 			
 			c.setMappaOrario(h);
-			listCorsi.add(c);
+			listCorsi.put(c.getCod(), c);
+			mapDocenti.put(c.getDocente().getMatricola(), c.getDocente());
+			mapAule.put(a.getIdAula(), a);
 		}
 	}
 	
 
-	
+	/*
 	
 	@GET 
-	@Path("/{idCorso}")
+	@Path("/corsi/{idCorso}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Corso getCorso(@PathParam("idCorso") String idCorso) {
 		Iterator<Corso> i = listCorsi.iterator();
@@ -56,11 +66,62 @@ public class EasyCourse {
 		return null;
 			
 	}
-	
+	*/
 	
 	
 	@GET
+	@Path("/corsi")
 	@Produces(MediaType.APPLICATION_JSON)
+	public HashMap<String,Corso> getCorsi(){
+		return this.listCorsi;
+	}
+	
+	@GET
+	@Path("/corsi/{id_corso}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Corso getCorsi(@PathParam("id_corso") String id_corso){
+		return this.listCorsi.get(id_corso);
+	}
+	
+	
+	@POST
+	@Path("/corsi/{id_corso}")
+	public Response test(@FormParam("cod") String idCorso, @FormParam("nome") String nome, @FormParam("nomeDocente") String nomeDocente, @FormParam("cognomeDocente") String cognomeDocente,@FormParam("matDocente") String matDocente,@FormParam("semestre") int semestre,@FormParam("anno") int anno) {
+		listCorsi.put(idCorso,new Corso(idCorso,nome,new Docente(nomeDocente,cognomeDocente,matDocente),semestre,anno));
+		String output = "POST REQUEST: " + idCorso;
+		return Response.status(200).entity(output).build();
+		
+	}
+	
+	
+	@GET
+	@Path("/docenti")
+	@Produces(MediaType.APPLICATION_JSON)
+	public HashMap<String,Docente> getDocenti(){
+		return this.mapDocenti;
+	}
+	@GET
+	@Path("/docenti/{id_docente}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Docente getDocente(@PathParam("id_docente") String id_docente){
+		return this.mapDocenti.get(id_docente);
+	}
+	
+	@GET
+	@Path("/aule")
+	@Produces(MediaType.APPLICATION_JSON)
+	public HashMap<Integer,Aula> getAule(){
+		return this.mapAule;
+	}
+	
+	@GET
+	@Path("/aule/{id_aula}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Aula getAula(@PathParam("id_aula") String id_aula){
+		return this.mapAule.get(id_aula);
+	}
+	
+	/*
 	public ArrayList<Corso> getCorsi(@QueryParam("da") int da, @QueryParam("a") int a){
 		
 		if(da == 0 && a==0)
@@ -101,7 +162,7 @@ public class EasyCourse {
 	
 	
 	@POST
-	@Path("/{corso}/")
+	@Path("/corsi/{corso}/")
 	//@Consumes(MediaType.TEXT_PLAIN)
 	public Response addCorso(@PathParam("corso") String idCorso) {
 		listCorsi.add(new Corso(idCorso,idCorso));
@@ -111,7 +172,7 @@ public class EasyCourse {
 	}
 	
 	@POST
-	@Path("/test")
+	@Path("/corsi/test")
 	//@Consumes(MediaType.)
 	public Response test(@FormParam("cod") String idCorso, @FormParam("nome") String nome, @FormParam("nomeDocente") String nomeDocente, @FormParam("cognomeDocente") String cognomeDocente,@FormParam("matDocente") String matDocente,@FormParam("semestre") int semestre,@FormParam("anno") int anno) {
 		listCorsi.add(new Corso(idCorso,nome,new Docente(nomeDocente,cognomeDocente,matDocente),semestre,anno));
@@ -135,7 +196,7 @@ public class EasyCourse {
 		}
 		return list;
 		
-	}
+	}*/
 	
 	
 }
