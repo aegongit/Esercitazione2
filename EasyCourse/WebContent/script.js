31353c307ce218d8458706d57308a97b31dc3030
michele <rmichele89@gmail.com>
2018-05-16 15:09:21 +0200

    ok

--- EasyCourse/WebContent/script.js
+++ EasyCourse/WebContent/script.js
@@ -34,11 +34,12 @@ formOrario.addEventListener("submit", function(e) {
 	http.onreadystatechange = function() {//Call a function when the state changes.
 	    if(http.readyState == 4 && http.status == 200) {
 	        alert(http.responseText);
+	        
 	    }
 	}
 	http.send(params);
 	e.preventDefault(); 
-	return;
+	
 });
 
 myForm.addEventListener("submit", function(e) {
